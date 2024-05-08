a0451d109f30f7ebf3a9d4fb5a97f6ef15ddc530
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-05-07 22:53:27 -0700

    added project

---
+++ Study.ConsoleP.Sports/Study.ConsoleP.Sports/Program.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+
+namespace Study.ConsoleP.Sports
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://example.com/");
+
+            var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+            var description = document.DocumentNode.SelectNodes("//div/p").First().InnerText;
+
+            Console.WriteLine(title);
+            Console.WriteLine();
+            Console.WriteLine(description); 
+
+
+        }
+    }
+}
